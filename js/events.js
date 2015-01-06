187640887ce4c57ed42473ce138ea92625aaf939
Aditya Balwani <adibalwani@gmail.com>
2015-01-06 19:00:30 +0530

    Events Page completed

---
+++ js/events.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Aditya on 1/6/2015.
+ */
+function setDataWedding() {
+	document.getElementById("eventName").innerHTML = "The Wedding";
+	document.getElementById("eventDay").innerHTML = "SUN";
+	document.getElementById("eventDate").innerHTML = "JAN 25";
+	document.getElementById("eventTime").innerHTML = "8:00pm";
+	document.getElementById("eventAddress").innerHTML = "Crystal Carnation,<br>	Mapple,<br>	Jaipur National Highway (NH 8), Rajokri, <br>	New Delhi <br>	India, 110038";
+	document.getElementById("mapLink").href = "https://www.google.co.in/maps/place/Mapple/@28.515227,77.094076,17z/data=!3m1!4b1!4m2!3m1!1s0x390d1bfe4fdeeac1:0x34beed0a9de74590?hl=en";
+	document.getElementById("mapLinkImage").href = "https://www.google.co.in/maps/place/Mapple/@28.515227,77.094076,17z/data=!3m1!4b1!4m2!3m1!1s0x390d1bfe4fdeeac1:0x34beed0a9de74590?hl=en";
+	document.getElementById("schedule").innerHTML = "Reception of Barat : 8:00pm <br>	Followed by Jaimala and Dinner : 8:30pm onwards";
+}
+
+function setDataPathSahib() {
+	document.getElementById("eventName").innerHTML = "Akhand Path Sahib";
+	document.getElementById("eventDay").innerHTML = "FRI";
+	document.getElementById("eventDate").innerHTML = "JAN 16";
+	document.getElementById("eventTime").innerHTML = "11am";
+	document.getElementById("eventAddress").innerHTML = "Shah ji Ka Gurudwara <br>	I - 11 <br>	Lajpat Nagar III<br>	New Delhi, India 110024";
+	document.getElementById("mapLink").href = "https://www.google.co.in/maps/place/I11,+Block+I,+Lajpat+Nagar+III,+Lajpat+Nagar,+New+Delhi,+Delhi+110024/@28.5671491,77.2400761,18z/data=!3m1!4b1!4m2!3m1!1s0x390ce24d4ec88383:0x736cdf02835ab072?hl=en";
+	document.getElementById("mapLinkImage").href = "https://www.google.co.in/maps/place/I11,+Block+I,+Lajpat+Nagar+III,+Lajpat+Nagar,+New+Delhi,+Delhi+110024/@28.5671491,77.2400761,18z/data=!3m1!4b1!4m2!3m1!1s0x390ce24d4ec88383:0x736cdf02835ab072?hl=en";
+	document.getElementById("schedule").innerHTML = "Start of Path Sahib : January 16, 2015 at 11:00am <br/>	Chola changing ceremony of Nishan Sahib : January 17, 2015 at 9:00am <br/>	Samapti of Path Sahib and Bhog : January 18, 2015 at 11:00am <br>	Followed by Kirtan and Langar";
+}
+
+function setDataMehendi() {
+	document.getElementById("eventName").innerHTML = "Mehendi";
+	document.getElementById("eventDay").innerHTML = "FRI";
+	document.getElementById("eventDate").innerHTML = "JAN 23";
+	document.getElementById("eventTime").innerHTML = "4:00pm";
+	document.getElementById("eventAddress").innerHTML = "House Number 363 <br>	D1 Flats, Block D 2, <br>	Vinay Marg <br>	Chanakyapuri<br>	New Delhi, India 110021";
+	document.getElementById("mapLink").href = "https://www.google.co.in/maps/place/D-II+364,+D1+Flats,+Block+D+2,+Chanakyapuri,+New+Delhi,+Delhi+110021/@28.586911,77.194236,18z/data=!3m1!4b1!4m6!1m3!3m2!1s0x390ce2831bfbe635:0x1edce69cbf7377bd!2sChanakyapuri!3m1!1s0x390d1d62ab743b6d:0x18c0cbe698a3b072?hl=en";
+	document.getElementById("mapLinkImage").href = "https://www.google.co.in/maps/place/D-II+364,+D1+Flats,+Block+D+2,+Chanakyapuri,+New+Delhi,+Delhi+110021/@28.586911,77.194236,18z/data=!3m1!4b1!4m6!1m3!3m2!1s0x390ce2831bfbe635:0x1edce69cbf7377bd!2sChanakyapuri!3m1!1s0x390d1d62ab743b6d:0x18c0cbe698a3b072?hl=en";
+	document.getElementById("schedule").innerHTML = "4:00pm onwards <br>	Followed by Music, Dance and Dinner";
+}
+
+function setDataRing() {
+	document.getElementById("eventName").innerHTML = "Ring Ceremony";
+	document.getElementById("eventDay").innerHTML = "SAT";
+	document.getElementById("eventDate").innerHTML = "JAN 24";
+	document.getElementById("eventTime").innerHTML = "7:00pm";
+	document.getElementById("eventAddress").innerHTML = "Hotel Airport Residency <br>Dwarka Link Road, Off NH 8, <br>Old Gurgaon Road, Samalkha, <br>New Delhi, India 110037";
+	document.getElementById("mapLink").href = "https://www.google.co.in/maps/place/Hotel+Airport+Residency/@28.53558,77.099454,17z/data=!3m1!4b1!4m2!3m1!1s0x390d1bf6c03f40ab:0xf7cdc5fe939b5d9d?hl=en";
+	document.getElementById("mapLinkImage").href = "https://www.google.co.in/maps/place/Hotel+Airport+Residency/@28.53558,77.099454,17z/data=!3m1!4b1!4m2!3m1!1s0x390d1bf6c03f40ab:0xf7cdc5fe939b5d9d?hl=en";
+	document.getElementById("schedule").innerHTML = "Godh Bharai : 7:00pm <br/>	Ring Ceremony: 8:00pm <br/>	Followed By Cocktail, Music and Dance";
+}
